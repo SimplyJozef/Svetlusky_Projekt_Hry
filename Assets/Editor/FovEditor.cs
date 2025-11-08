38d177ef20a48e014f3e02e2ca227b7a3ee3b72f
Nevermind3005 <jakub.elis007@gmail.com>
2025-11-08 21:07:08 +0100

    Add simple ai follow player

---
+++ Assets/Editor/FovEditor.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MonsterController))]
+public class FovEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        var monster = (MonsterController)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(monster.transform.position, Vector3.up, Vector3.forward, 360, monster.VisionRadius);
+
+        var viewAngleLeft = DirFromAngle(monster.transform.eulerAngles.y, -monster.VisionAngle / 2);
+        var viewAngleRight = DirFromAngle(monster.transform.eulerAngles.y, monster.VisionAngle / 2);
+
+        Handles.color = Color.yellow;
+        Handles.DrawLine(monster.transform.position, monster.transform.position + viewAngleLeft * monster.VisionRadius);
+        Handles.DrawLine(monster.transform.position, monster.transform.position + viewAngleRight * monster.VisionRadius);
+
+        if (monster.bCanSeeTarget)
+        {
+            Handles.color = Color.green;
+            Handles.DrawLine(monster.transform.position, monster.transform.position + viewAngleRight * monster.VisionRadius);
+        }
+    }
+
+    private Vector3 DirFromAngle(float eulerY, float angleInDeg)
+    {
+        angleInDeg += eulerY;
+
+        return new Vector3(Mathf.Sin(angleInDeg * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDeg * Mathf.Deg2Rad));
+    }
+}
